83e88cf1800d3a27c4880c3b885771bd0fbf5162
Sheetal Wagh <sheetalwagh85@gmail.com>
2012-02-21 15:10:08 +0530

    added basic app with authentication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AskQuestion::Application.config.secret_token = '4e8517972fd781fa306cd67dd006c8b1da2244491cc8b38365d28974c073aebe297a0e9405ab85d474f410f7f08f09439c7b498c106ce2e3ec1ae46d062106de'
